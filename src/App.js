7a64796b82e7ebb45a316f96ef16da97d3e91bbe
jchengjr77 <jcheng3@andrew.cmu.edu>
2019-07-09 11:15:25 +0800

    Restructured project for react app.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './App.css';
-import Header from "./Header.jsx";
-import Counters from "./Counters.jsx";
+import './css/App.css';
+import Header from "./components/Header.jsx";
+import Counters from "./components/Counters.jsx";
 
 function App() {
   return (
